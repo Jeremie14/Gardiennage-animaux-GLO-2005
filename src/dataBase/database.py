2dd391a8bcdcb274af5e5f01834674357087be60
Jeremie <jerpelletier13@icloud.com>
2026-04-09 18:54:46 -0400

    Addition of the hashed passwords

--- src/dataBase/database.py
+++ src/dataBase/database.py
@@ -3,8 +3,8 @@ import pymysql.cursors
 connection = pymysql.connect(
     host='localhost',
     user='root',
-   # password='PabloIsCrazy123',
-    password='Aymen12345',
+    password='PabloIsCrazy123',
+    # password='Aymen12345',
     db='gardiennage_animaux_GLO2005_2026',
     autocommit=True
 )
